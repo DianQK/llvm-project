ea49b84a5a28cf1f16280f1dca0b6a2b2f97f0d0
DianQK <dianqk@dianqk.net>
2024-02-05 21:16:58 +0800

    [TailDuplicator] Add maximum predecessors and successors to consider tail duplicating blocks

--- llvm/lib/CodeGen/TailDuplicator.cpp
+++ llvm/lib/CodeGen/TailDuplicator.cpp
@@ -68,6 +68,18 @@ static cl::opt<unsigned> TailDupIndirectBranchSize(
              "end with indirect branches."), cl::init(20),
     cl::Hidden);
 
+static cl::opt<unsigned>
+    TailDupSuccSize("tail-dup-succ-size",
+                    cl::desc("Maximum successors (Maximum predecessors at the "
+                             "same time) to consider tail duplicating blocks."),
+                    cl::init(16), cl::Hidden);
+
+static cl::opt<unsigned>
+    TailDupPredSize("tail-dup-pred-size",
+                    cl::desc("Maximum predecessors (Maximum successors at the "
+                             "same time) to consider tail duplicating blocks."),
+                    cl::init(16), cl::Hidden);
+
 static cl::opt<bool>
     TailDupVerify("tail-dup-verify",
                   cl::desc("Verify sanity of PHI instructions during taildup"),
@@ -563,6 +575,10 @@ bool TailDuplicator::shouldTailDuplicate(bool IsSimple,
 
   // Don't try to tail-duplicate single-block loops.
   if (TailBB.isSuccessor(&TailBB))
+    return false;
+
+  if (TailBB.succ_size() > TailDupSuccSize &&
+      TailBB.pred_size() > TailDupPredSize)
     return false;
 
   // Set the limit on the cost to duplicate. When optimizing for size,
